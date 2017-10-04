bdecd28db192d38541313b3464b893822292195a
Jan Vargovský <jan.vargovsky@gmail.com>
2017-10-04 13:24:58 +0200

    Add project files.

---
+++ MAD.Lesson3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MAD.Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
