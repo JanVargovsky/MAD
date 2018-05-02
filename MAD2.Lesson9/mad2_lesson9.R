7accd795191b5d9e4d60c0f55f89422ca73733a1
Jan Vargovský <jan.vargovsky@gmail.com>
2018-05-02 20:23:46 +0200

    Lesson 9 - plot progress of max. cluster during removing nodes with highest degree and random one.

--- MAD2.Lesson9/mad2_lesson9.R
+++ MAD2.Lesson9/mad2_lesson9.R
@@ -1,3 +1,4 @@
+library(igraph)
 # igraph
 N = 5500
 M = 3
@@ -34,6 +35,7 @@ mean(degree(g2))
 
 # 3
 print_info <- function(g){
+  print(paste("No nodes", vcount(g)))
   print(paste("No clusters", no.clusters(g)))
   print(paste("Clusters ", clusters(g)$csize))
   print(paste("Max Cluster ", max(clusters(g)$csize)))
@@ -57,27 +59,61 @@ remove_random_node <- function(g){
 }
 
 # c
-
 call_3 <- function(g){
+  remove_nodes = 5
   g_m = g
   print_info(g_m)
-  for(i in 0:10) {
+  for(i in 0:remove_nodes) {
     g_m = remove_node_with_highest_degree(g_m)
+    print('========================================================================================================================')
     print_info(g_m)
   }
   
+  g_m = g
   print_info(g_m)
-  for(i in 0:10) {
+  for(i in 0:remove_nodes) {
     g_m = remove_random_node(g_m)
+    print('========================================================================================================================')
     print_info(g_m)
   }
 }
 
-call_3(g1)
-
-g1_m = g1
-print_info(g1_m)
-for(i in 0:10) {
-  g1_m = remove_node_with_highest_degree(g1_m)
-  print_info(g1_m)
+call_5 <- function(g, title) {
+  remove_nodes = vcount(g) - 1
+  step = 100
+  
+  g_m = g
+  values = c()
+  values = append(values, max(clusters(g)$csize))
+  nodes = c()
+  nodes = append(nodes, 1)
+  for(i in 1:remove_nodes/step){
+    for (j in step){
+      g_m = remove_node_with_highest_degree(g_m)
+    }
+    values = append(values, max(clusters(g_m)$csize))
+    nodes = append(nodes, i*step)
+  }
+  plot(nodes, values, xlab = "pocet odstranenych vrcholu (s nejvetsim stupnem)", ylab="velikost maximalniho clusteru", main = title)
+  
+  g_m = g
+  values = c()
+  values = append(values, max(clusters(g_m)$csize))
+  nodes = c()
+  nodes = append(nodes, 1)
+  for(i in 1:remove_nodes/step){
+    for (j in step){
+      g_m = remove_random_node(g_m)
+    }
+    values = append(values, max(clusters(g_m)$csize))
+    nodes = append(nodes, i*step)
+  }
+  plot(nodes, values, xlab = "pocet odstranenych vrcholu (nahodny)", ylab="velikost maximalniho clusteru", main = title)
 }
+
+call_3(g1)
+call_3(g2)
+
+call_5(g1, "Barabasi-Albert Model")
+call_5(g2, "Nahodny graf")
+
