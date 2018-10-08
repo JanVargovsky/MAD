c700b7ec2f5e238d66305ce03ad9adaf9a86ca8a
Jan Vargovský <jan.vargovsky@gmail.com>
2018-10-08 20:08:28 +0200

    Lesson2 - calculating minimal entropy. Row/Column dataset.

--- MAD3.Lesson2/Dataset.cs
+++ MAD3.Lesson2/Dataset.cs
@@ -3,18 +3,73 @@ using System.Linq;
 
 namespace MAD3.Lesson2
 {
-    public class Dataset
+    public interface IDataset
     {
-        public List<double[]> Data { get; }
+        double this[int row, int column] { get; }
+        int NumberOfAttributes { get; }
+        int Count { get; }
 
-        public int NumberOfAttributes => Data[0].Length;
+        double MinOf(int column);
+        double MaxOf(int column);
+    }
 
-        public Dataset()
+    public class RowDataset : IDataset
+    {
+        public double this[int row, int column] => Rows[row][column];
+        public List<double[]> Rows { get; }
+        public int NumberOfAttributes => Rows[0].Length;
+        public int Count => Rows.Count;
+
+        public RowDataset()
         {
-            Data = new List<double[]>();
+            Rows = new List<double[]>();
         }
 
-        public double MinOf(int i) => Data.Min(t => t[i]);
-        public double MaxOf(int i) => Data.Max(t => t[i]);
+        public void Add(double[] values)
+        {
+            Rows.Add(values);
+        }
+
+        public double MinOf(int column) => Rows.Min(t => t[column]);
+        public double MaxOf(int column) => Rows.Max(t => t[column]);
+    }
+
+    public class ColumnDataset : IDataset
+    {
+        public double this[int row, int column] => Columns[column][row];
+        public List<double>[] Columns { get; }
+        public int NumberOfAttributes => Columns.Length;
+        public int Count => Columns[0].Count;
+
+        public ColumnDataset(int columns)
+        {
+            Columns = new List<double>[columns];
+            for (int i = 0; i < columns; i++)
+                Columns[i] = new List<double>();
+        }
+
+        public ColumnDataset(List<double>[] columns) => Columns = columns;
+
+        public void Add(double[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+                Columns[i].Add(values[i]);
+        }
+
+        public double MinOf(int column) => Columns[column].Min();
+        public double MaxOf(int column) => Columns[column].Max();
+    }
+
+    public static class RowDatasetExtensions
+    {
+        public static ColumnDataset AsColumnDataset(this RowDataset dataset)
+        {
+            var columnDataset = new ColumnDataset(dataset.NumberOfAttributes);
+
+            foreach (var row in dataset.Rows)
+                columnDataset.Add(row);
+
+            return columnDataset;
+        }
     }
 }
