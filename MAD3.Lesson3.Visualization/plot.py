60b3ab780375ae23bf516df919999a460a1a51b6
Jan Vargovský <jan.vargovsky@gmail.com>
2018-10-14 20:36:22 +0200

    Lesson3 - improve plots quality.

--- MAD3.Lesson3.Visualization/plot.py
+++ MAD3.Lesson3.Visualization/plot.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 FILENAMES = ("clusters3.csv", "clusters5.csv", "clusters5n.csv", "annulus.csv", "boxes.csv")
 ALGORITHMS = ("SingleLinkage", "CompleteLinkage")
 COLUMN_NAMES = ('x', 'y', 'class')
-COLORS = ("b", "g", "r", "c", "m")
+COLORS = ("b", "g", "r", "y", "m")
 
 for FILENAME in FILENAMES:
     for (i, ALGORITHM) in enumerate(ALGORITHMS):
@@ -13,8 +12,9 @@ for FILENAME in FILENAMES:
         frame = pd.read_csv(filename, delimiter=';', encoding='windows-1250', names=COLUMN_NAMES)
         grouped = frame.groupby('class')
         plt.subplot(2, 1, i+1)
-        plt.title(filename)
+        plt.title('{0} - {1}'.format(FILENAME, ALGORITHM))
+        plt.gcf().set_size_inches(5, 10)
         for ((_, group), color) in zip(grouped, COLORS):
             plt.scatter(group['x'], group['y'], c=color, marker=".")
-    plt.savefig('Data/Export-{0}.png'.format(FILENAME))
+    plt.savefig('Data/Export-{0}.png'.format(FILENAME), dpi=300)
     plt.show()
